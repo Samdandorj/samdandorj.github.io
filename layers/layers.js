86cec66cc18c1d2e99b507ff15a30a3bb89f4c19
Samdandorj <73830630+Samdandorj@users.noreply.github.com>
2020-11-02 13:22:28 +0100

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,120 @@
+var wms_layers = [];
+
+var format_lake_0 = new ol.format.GeoJSON();
+var features_lake_0 = format_lake_0.readFeatures(json_lake_0, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_lake_0 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_lake_0.addFeatures(features_lake_0);
+var lyr_lake_0 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_lake_0, 
+                style: style_lake_0,
+                interactive: true,
+                title: '<img src="styles/legend/lake_0.png" /> lake'
+            });
+var format_river_1 = new ol.format.GeoJSON();
+var features_river_1 = format_river_1.readFeatures(json_river_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_river_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_river_1.addFeatures(features_river_1);
+var lyr_river_1 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_river_1, 
+                style: style_river_1,
+                interactive: true,
+                title: '<img src="styles/legend/river_1.png" /> river'
+            });
+var format_csongmat_2 = new ol.format.GeoJSON();
+var features_csongmat_2 = format_csongmat_2.readFeatures(json_csongmat_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_csongmat_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_csongmat_2.addFeatures(features_csongmat_2);
+var lyr_csongmat_2 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_csongmat_2, 
+                style: style_csongmat_2,
+                interactive: true,
+    title: 'csongmat<br />\
+    <img src="styles/legend/csongmat_2_0.png" /> Csongrad<br />\
+    <img src="styles/legend/csongmat_2_1.png" /> Szentes<br />\
+    <img src="styles/legend/csongmat_2_2.png" /> Kistelek<br />\
+    <img src="styles/legend/csongmat_2_3.png" /> Hodmezovasarhely<br />\
+    <img src="styles/legend/csongmat_2_4.png" /> Morahalom<br />\
+    <img src="styles/legend/csongmat_2_5.png" /> Szeged<br />\
+    <img src="styles/legend/csongmat_2_6.png" /> Mako<br />'
+        });
+var format_road_3 = new ol.format.GeoJSON();
+var features_road_3 = format_road_3.readFeatures(json_road_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_road_3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_road_3.addFeatures(features_road_3);
+var lyr_road_3 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_road_3, 
+                style: style_road_3,
+                interactive: true,
+    title: 'road<br />\
+    <img src="styles/legend/road_3_0.png" /> Primary<br />\
+    <img src="styles/legend/road_3_1.png" /> Secondary<br />\
+    <img src="styles/legend/road_3_2.png" /> 5<br />'
+        });
+var format_settlement_4 = new ol.format.GeoJSON();
+var features_settlement_4 = format_settlement_4.readFeatures(json_settlement_4, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_settlement_4 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_settlement_4.addFeatures(features_settlement_4);
+var lyr_settlement_4 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_settlement_4, 
+                style: style_settlement_4,
+                interactive: true,
+                title: '<img src="styles/legend/settlement_4.png" /> settlement'
+            });
+var format_county_5 = new ol.format.GeoJSON();
+var features_county_5 = format_county_5.readFeatures(json_county_5, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_county_5 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_county_5.addFeatures(features_county_5);
+var lyr_county_5 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_county_5, 
+                style: style_county_5,
+                interactive: true,
+                title: '<img src="styles/legend/county_5.png" /> county'
+            });
+
+lyr_lake_0.setVisible(true);lyr_river_1.setVisible(true);lyr_csongmat_2.setVisible(true);lyr_road_3.setVisible(true);lyr_settlement_4.setVisible(true);lyr_county_5.setVisible(true);
+var layersList = [lyr_lake_0,lyr_river_1,lyr_csongmat_2,lyr_road_3,lyr_settlement_4,lyr_county_5];
+lyr_lake_0.set('fieldAliases', {'AREA': 'AREA', 'PERIMETER': 'PERIMETER', 'TO_': 'TO_', 'TO_ID': 'TO_ID', 'BENT': 'BENT', });
+lyr_river_1.set('fieldAliases', {'AREA': 'AREA', 'PERIMETER': 'PERIMETER', 'FOLYO_': 'FOLYO_', 'FOLYO_ID': 'FOLYO_ID', 'BENT': 'BENT', });
+lyr_csongmat_2.set('fieldAliases', {'AREA': 'AREA', 'PERIMETER': 'PERIMETER', 'TELHAT_ID': 'TELHAT_ID', 'TELNEV': 'TELNEV', 'KSHKOD': 'KSHKOD', 'TELNAGYSAG': 'TELNAGYSAG', 'KISTERSEG': 'KISTERSEG', 'AGGLO': 'AGGLO', });
+lyr_road_3.set('fieldAliases', {'UTAK_': 'UTAK_', 'UTAK_ID': 'UTAK_ID', 'DXF_LAYER': 'DXF_LAYER', 'REND': 'REND', 'FORG': 'FORG', });
+lyr_settlement_4.set('fieldAliases', {'AREA': 'AREA', 'PERIMETER': 'PERIMETER', 'BELTER_': 'BELTER_', 'BELTER_ID': 'BELTER_ID', 'TELNEV': 'TELNEV', });
+lyr_county_5.set('fieldAliases', {'AREA': 'AREA', 'PERIMETER': 'PERIMETER', 'MEGYE_': 'MEGYE_', 'MEGYE_ID': 'MEGYE_ID', 'M': 'M', });
+lyr_lake_0.set('fieldImages', {'AREA': '', 'PERIMETER': '', 'TO_': '', 'TO_ID': '', 'BENT': '', });
+lyr_river_1.set('fieldImages', {'AREA': '', 'PERIMETER': '', 'FOLYO_': '', 'FOLYO_ID': '', 'BENT': '', });
+lyr_csongmat_2.set('fieldImages', {'AREA': '', 'PERIMETER': '', 'TELHAT_ID': '', 'TELNEV': '', 'KSHKOD': '', 'TELNAGYSAG': '', 'KISTERSEG': '', 'AGGLO': '', });
+lyr_road_3.set('fieldImages', {'UTAK_': '', 'UTAK_ID': '', 'DXF_LAYER': '', 'REND': '', 'FORG': '', });
+lyr_settlement_4.set('fieldImages', {'AREA': '', 'PERIMETER': '', 'BELTER_': '', 'BELTER_ID': '', 'TELNEV': '', });
+lyr_county_5.set('fieldImages', {'AREA': '', 'PERIMETER': '', 'MEGYE_': '', 'MEGYE_ID': '', 'M': '', });
+lyr_lake_0.set('fieldLabels', {'AREA': 'no label', 'PERIMETER': 'no label', 'TO_': 'no label', 'TO_ID': 'no label', 'BENT': 'no label', });
+lyr_river_1.set('fieldLabels', {'AREA': 'no label', 'PERIMETER': 'no label', 'FOLYO_': 'no label', 'FOLYO_ID': 'no label', 'BENT': 'no label', });
+lyr_csongmat_2.set('fieldLabels', {'AREA': 'no label', 'PERIMETER': 'no label', 'TELHAT_ID': 'no label', 'TELNEV': 'no label', 'KSHKOD': 'no label', 'TELNAGYSAG': 'no label', 'KISTERSEG': 'no label', 'AGGLO': 'no label', });
+lyr_road_3.set('fieldLabels', {'UTAK_': 'no label', 'UTAK_ID': 'no label', 'DXF_LAYER': 'no label', 'REND': 'no label', 'FORG': 'no label', });
+lyr_settlement_4.set('fieldLabels', {'AREA': 'no label', 'PERIMETER': 'no label', 'BELTER_': 'no label', 'BELTER_ID': 'no label', 'TELNEV': 'no label', });
+lyr_county_5.set('fieldLabels', {'AREA': 'inline label', 'PERIMETER': 'header label', 'MEGYE_': 'no label', 'MEGYE_ID': 'no label', 'M': 'no label', });
+lyr_county_5.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ styles/csongmat_2_style.js
@@ -0,0 +1,88 @@
+var size = 0;
+var placement = 'point';
+function categories_csongmat_2(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {case '1':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(234,209,45,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(139,202,100,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '3':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(207,48,26,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '4':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(120,231,161,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '5':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(74,191,206,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '6':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(162,60,224,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '7':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(106,121,219,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_csongmat_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("KISTERSEG");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_csongmat_2(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
